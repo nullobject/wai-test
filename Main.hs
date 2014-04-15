9e5f120671c418c29e66b45cf78793dbe10e6491
Josh Bassett <josh.bassett@gmail.com>
2014-04-15 17:26:13 +1000

    Initial LOLs.

---
+++ Main.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
+
+app :: Application
+app _ = do
+  putStrLn "I've done some IO here"
+  return $ responseLBS
+    status200
+    [("Content-Type", "text/plain")]
+    "Hello, Web!"
+
+main :: IO ()
+main = do
+    putStrLn $ "http://localhost:8080/"
+    run 8080 app
