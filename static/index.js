e800330020ecb7d2965579f8f9fdd19d600c8b50
Josh Bassett <josh.bassett@gmail.com>
2014-04-23 21:48:34 +1000

    Cleanups.

--- static/index.js
+++ static/index.js
@@ -6,8 +6,7 @@ var output = dom('.container p');
 var eventSource = new EventSource('/eschan');
 
 var messages = Bacon.fromEventTarget(eventSource, 'message')
-  .map(function (e) { return parseInt(e.data); })
-  .map(function (value) { return new Date(value); });
+  .map(function (e) { return new Date(parseInt(e.data)); });
 
 messages.onValue(function (message) {
   output.text(message);
